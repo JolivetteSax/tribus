cb0d491b7ab3f046f073d8422e6c78fbd4161b8e
esax <erikjsax@gmail.com>
2023-11-09 21:59:38 -0500

    basics of login logic

--- backend/src/login.js
+++ backend/src/login.js
@@ -7,14 +7,10 @@ const moment = require('moment');
 const User = require('../models/Users.js');
 
 router.post('/login', (req, res) => {
-  console.log(req.headers);
-  console.log('Body');
-  console.log(req.body);
   if(! (req.body.email && req.body.hash && req.body.timestamp)){
     return res.status(401).send({error: 'missing credentials', authorized: false});
   }
   let email = req.body.email;
-  let hash;
   User.findOne({
     email,
   })
@@ -23,23 +19,22 @@ router.post('/login', (req, res) => {
       console.log(email + ' Not found');
       return false;
     }
-    hash = md5(req.body.timestamp + user.passhash);
-    console.log(hash);
-    return user;
-    //if(hash === req.body.hash){
-    //  email = user.email;
-    //  return true;
-    //}
-    //return false;
+    let hash = md5(req.body.timestamp + user.passhash);
+    if(hash === req.body.hash){
+      const token = md5(moment().utc().format() + hash);
+      user.token = token;
+      return user;
+    }
+    else{
+      return false;
+    }
   })
   .then((user) => {
     if(user){
-      const token = md5(moment().utc().format() + hash);
-      user.token = token;
       user.save()
       .then(() => {
         res.set('X-Arcis-Username', user.email);
-        return res.status(200).send({authorized: true, token});
+        return res.status(200).send({authorized: true, token: user.token});
       });
     }
     else{
